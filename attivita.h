861a2581964615ace1660aeeaef05058dd75a767
zapytaylo <c.zapytaylo@studenti.unisa.it>
2025-06-13 01:01:38 +0200

    attivita.h

--- attivita.h
+++ attivita.h
@@ -1,12 +1,46 @@
-#define LUNGHEZZA_DC 50
-#define L_DATA 10
+#ifndef ATTIVITA_H
+#define ATTIVITA_H
 
+// Costanti di lunghezza per le stringhe
+#define DESC_LEN 100
+#define CORSO_LEN 50
+#define DATA_LEN 11 // Data nel formato "AAAA-MM-GG" + terminatore
+
+// Enumerazioni per la priorità e lo stato di un'attività
+typedef enum { BASSA = 1, MEDIA, ALTA } Priorita;
+typedef enum { IN_CORSO, COMPLETATA, RITARDO } Stato;
+
+// Struttura che rappresenta un'attività
 typedef struct {
-    char descr[LUNGHEZZA_DC];
-    char corso[LUNGHEZZA_DC];
-    int data[L_DATA];
-    int tempo;
-    int priorita;
-    char completato;
-} attivita;
+    char descrizione[DESC_LEN];
+    char corso[CORSO_LEN];
+    char scadenza[DATA_LEN];
+    int tempoStimato;
+    int tempoTrascorso;
+    Priorita priorita;
+    Stato stato;
+} Attivita;
 
+// Nodo della lista collegata
+typedef struct Nodo {
+    Attivita attivita;
+    struct Nodo* next;
+} Nodo;
+
+// Funzioni del programma
+Nodo* aggiungiAttivita(Nodo* testa);
+void mostraAttivita(Nodo* testa);
+void aggiornaProgresso(Nodo* testa);
+void generaReport(Nodo* testa);
+void liberaLista(Nodo* testa);
+void mostraMenu();
+
+
+// Nuove funzioni con supporto file
+Nodo* aggiungiAttivitaFile(Nodo* testa, FILE* input, FILE* output);
+void mostraAttivitaFile(Nodo* testa, FILE* input, FILE* output);
+void aggiornaProgressoFile(Nodo* testa, FILE* input, FILE* output);
+void generaReportFile(Nodo* testa, FILE* input, FILE* output);
+void mostraMenuFile(FILE* output);
+
+#endif
